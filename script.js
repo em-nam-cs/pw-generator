784018c71b2060c1ca441a2cb0be665b3763ee7d
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2024-04-02 11:51:43 -0700

    Finished basic styling

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+/**
+@brief basic password generator that creates a random string password
+    with the given conditions (uppercase, numbers, symbols, length)
+
+@references https://www.youtube.com/watch?v=iKo9pDKKHnc
+@author Em Nam
+@date 04-02-2024
+ */
